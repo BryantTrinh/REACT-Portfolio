be818e0cad9ea5a288f229485d4dea1c4c666e4a
Bryant Trinh <trinhbryant@gmail.com>
2023-01-25 15:40:12 -0800

    moved files around and added an about me

---
+++ src/components/About/index.js
@@ -0,0 +1,27 @@
+
+function About() {
+
+  return(
+
+    <section className="my-5">
+
+      <div className="my-2">
+
+        <div className="profile-img my-5">
+
+          <i className="fas fa-user-circle" style={{ fontSize: "85px" }}></i>
+          
+        </div>
+
+        <p>Sint amet anim sint reprehenderit esse nostrud ut voluptate ea enim cupidatat anim aute ipsum. Consectetur non minim exercitation enim quis incididunt non. Sit dolor aute id adipisicing aliqua elit consequat sint aliqua minim laboris sint ipsum nisi. Aliqua enim sit est laboris eiusmod Lorem reprehenderit Lorem duis quis. Ad laborum non magna excepteur irure amet dolore minim. Culpa excepteur occaecat amet mollit ex.</p>
+
+      <p>
+        Sint do laboris consequat in ea. Voluptate officia exercitation ex enim duis nisi deserunt mollit culpa laboris occaecat exercitation culpa. Deserunt consectetur occaecat exercitation elit sint ea id. Laboris mollit mollit labore incididunt anim non. Tempor dolor Lorem Lorem incididunt quis aliquip magna irure velit incididunt ullamco.
+      </p>
+      </div>
+    </section>
+  );
+}
+
+
+export default About;
