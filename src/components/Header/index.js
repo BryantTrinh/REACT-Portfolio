8cdad106ac084835e8ff1cc2a299424c84dedae8
Bryant Trinh <trinhbryant@gmail.com>
2023-01-25 17:30:01 -0800

    header and nav done

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import coverImage from '../../assets/cover/cover-image.jpg';
+
+function Header(props) {
+  return (
+    <header className="flex-row space-between px-1">
+      <h1>Bryant Trinh- Full Stack Developer Portfolio</h1>
+      <img src={coverImage} alt="background"></img>
+      {props.children}
+    </header>
+  );
+}
