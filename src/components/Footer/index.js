6ac469ca649a371c5a97eb48d107814c5aea5c18
Bryant Trinh <trinhbryant@gmail.com>
2023-01-25 17:19:10 -0800

    footer finished

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+
+
+import React from 'react';
+
+function footer() {
+  const icons = [
+    {
+      name: "fab fa-github",
+      link: "https://github.com/BryantTrinh"
+    },
+    {
+      name: "fab fa-linkedin",
+      link: "https://www.linkedin.com/in/bryant-trinh-60208014a/"
+    },
+  ]
+
+  return (
+    <footer className="flex-row px-1">
+      {icons.map(icon => (
+        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
+      )
+        )}
+    </footer>
+  );
+  }
+
+  export default Footer;
